981ede3aa256f471f88c5e39a75aa8d92687d408
Didzis Gosko <didzis.gosko@gmail.com>
2016-08-01 23:51:01 +0300

    Updated rest.sh with reuse option.

--- rest.sh
+++ rest.sh
@@ -9,9 +9,11 @@ function usage {
 	echo "usage: $0 [--image NAME] [-p PORT] [--detach] [--name NAME] [--] [options for rest.py (see below)]"
 	echo
 	echo "--image NAME           specify docker image (default: $image)"
+	echo "--pull                 pull docker image from registry"
 	echo "-p PORT, --port PORT   specify port on host system (default: $port)"
 	echo "--name NAME            specify docker container name"
 	echo "-d, --detach           run container in background"
+	echo '-r, --reuse            reuse container if already exists (will first try `docker start NAME`)'
 	echo
 	echo "notes:"
 	echo "* requires up to 22GB of RAM"
@@ -26,6 +28,10 @@ function check_image {
 		if [[ $REPLY =~ ^[Nn]$ ]]; then
 			exit 1
 		fi
+	elif [ $pull -eq 1 ]; then
+		echo
+		echo "Pulling from registry"
+		docker pull $image
 	fi
 }
 
@@ -45,6 +51,8 @@ for arg in $@; do
 done
 
 detach_rm="--rm"
+reuse=0
+pull=0
 
 args=()
 container_args_only=0
@@ -59,10 +67,17 @@ while [ $# -gt 0 ]; do
         shift
     elif [ $container_args_only -ne 1 ] && [ "$1" = "--name" ]; then
 		shift
-		name="--name $1"
+		name="$1"
+		name_arg="--name $1"
         shift
 	elif [ $container_args_only -ne 1 ] && ([ "$1" = "--detach" ] || [ "$1" = "-d" ]); then
 		detach_rm="-d"
+        shift
+	elif [ $container_args_only -ne 1 ] && ([ "$1" = "--reuse" ] || [ "$1" = "-r" ]); then
+		reuse=1
+        shift
+	elif [ $container_args_only -ne 1 ] && [ "$1" = "--pull" ]; then
+		pull=1
         shift
 	elif [ $container_args_only -ne 1 ] && ([ "$1" = "--port" ] || [ "$1" = "-p" ]); then
 		shift
@@ -74,7 +89,26 @@ while [ $# -gt 0 ]; do
 	fi
 done
 
-echo Executing: docker run --rm -p $port:5000 $docker_tty $image --rest "${args[@]}"
+if [ $reuse -eq 1 ] && [ "$name" != "" ]; then
+	if [ "`docker ps -aq --format '{{.Names}}' | grep "^$name$"`" != "" ]; then
+		docker start $name
+		if [ $? -eq 0 ]; then
+			exit 0
+		else
+			exit 1
+		fi
+	else
+		echo "Container $name does not exist, will create."
+		echo
+	fi
+fi
+
+if [ $reuse -eq 1 ] && [ "$name" != "" ] && [ "$detach_rm" == "--rm" ]; then
+	# don't remove container if planned to be reused
+	detach_rm=
+fi
+
+echo Will execute: docker run $detach_rm $name_arg -p $port:5000 $docker_tty $image --rest "${args[@]}"
 check_image
 
 echo
@@ -82,4 +116,4 @@ echo REST API will be available at host IP port $port
 sleep 1
 echo
 
-docker run $detach_rm $name -p $port:5000 $docker_tty $image --rest "${args[@]}"
+docker run $detach_rm $name_arg -p $port:5000 $docker_tty $image --rest "${args[@]}"
